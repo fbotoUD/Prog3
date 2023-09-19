0f1d18ff4c22249e46d68f6aa8cb194105bb3b43
fernando.boto <fernando.boto@INGLT03607.deusto.es>
2023-09-19 10:55:50 +0200

    Ejercios genericidad

---
+++ Clase/src/es/deusto/prog3/cap00/ejercicios/Diccionario.java
@@ -0,0 +1,13 @@
+package es.deusto.prog3.cap00.ejercicios;
+
+/*
+ * Programa una clase Diccionario que permita gestionar
+ * un pequeño diccionario con palabras asociadas a definiciones
+ * (cada palabra puede tener varias definiciones).
+ * Usa la estructura de TreeMap combinada con ArrayList.
+ * 
+ */
+
+public class Diccionario {
+
+}
---
+++ Clase/src/es/deusto/prog3/cap00/ejercicios/Scores.java
@@ -0,0 +1,11 @@
+package es.deusto.prog3.cap00.ejercicios;
+
+/*
+ * Programa una clase Scores que permita gestionar usuarios con nicks
+ * únicos y guardar todas sus puntuaciones.
+ * Usa la estructura de HashMap combinada con ArrayList.
+ */
+
+public class Scores {
+
+}
