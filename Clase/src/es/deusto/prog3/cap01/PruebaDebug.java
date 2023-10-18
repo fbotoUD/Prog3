d192ca891a737ee61e485167a725d5d69171aa66
fernando.boto <fernando.boto@INGLT03607.deusto.es>
2023-10-18 09:27:06 +0200

    Modificación debug

--- Clase/src/es/deusto/prog3/cap01/PruebaDebug.java
+++ Clase/src/es/deusto/prog3/cap01/PruebaDebug.java
@@ -10,13 +10,32 @@ public class PruebaDebug {
 	private static void mensajeaNumeros(int i) {
 		for (int j=0; j<i; j++)
 			sacaMens( "Número " + j );
+		
+		
+	}
+	
+	private static void haceCosas() {
+		int a = 5;
+        int b = 7;
+        
+        int suma = a + b;
+        int resta = a - b;
+        int multiplicacion = a * b;
+        int division = a / b;
+        
+             
+        for (int i = 0; i < 10; i++) {
+            suma++;
+            miMens = null;
+            resta--;
+        }
 	}
 	private static void sacaMens( String mens ) {
 		System.out.println( "Mensaje: " + mens + " (longitud " + mens.length() + ")" );
 	}
 	public static void main(String[] args) {
 		mensajeaNumeros(7);
-		miMens = null;
+		haceCosas();
 		sacaMens(miMens);
 	}
 
