27b6fb2e44a46f91e0135c124425dddca1217643
fernando.boto <fernando.boto@INGLT03607.deusto.es>
2023-10-24 11:16:48 +0200

    ejercicio Junit

---
+++ Clase/test/es/deusto/prog3/cap01/ejercicios/TestOperacionesConArray.java
@@ -0,0 +1,32 @@
+package es.deusto.prog3.cap01.ejercicios;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/*
+ * 	1- Comprobar que sumarAPosicion() funciona como esperamos mediante un assertEquals().
+	2- Crear otro método de testeo para ver que el método sumarAPosicion() salta la excepción IndexOutOfBoundsException,
+	mediante la etiqueta @Test(expected = IndexOutOfBoundsException.class).
+	3- Realizar un método de test que compruebe que sumarPosiciones() funciona correctamente con una prueba unitaria.
+	4- Añadir otro método de testeo para comprobar que sumarPosiciones() lanza la excepción NullPointerException mediante un try catch y fail().
+ */
+public class TestOperacionesConArray {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
