8e66b8e791376845a8e3fe117596a031588bcdcf
fernando.boto <fernando.boto@INGLT03607.deusto.es>
2023-11-06 14:50:11 +0100

    quitar mouse listener

--- Clase/src/es/deusto/prog3/cap06/ProbandoJTableSimple.java
+++ Clase/src/es/deusto/prog3/cap06/ProbandoJTableSimple.java
@@ -174,41 +174,6 @@ public class ProbandoJTableSimple {
 			}
 			
 		});
-		
-		
-		
-		tabla.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 				
 		
 		vent.setVisible(true);
